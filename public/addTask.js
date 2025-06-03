dabbac3d5afac1b8d798ad791691581510c088bf
karolmazur730@gmail.comm <karolmazur730@gmail.com>
2025-06-04 01:29:31 +0200

    no siema

--- public/addTask.js
+++ public/addTask.js
@@ -4,7 +4,7 @@ addTaskButton.addEventListener('click', function() {
     const topic = document.getElementById('topicInput').value;
     const date = document.getElementById('dateInput').value;
     const description = document.getElementById('description').value;
-    if(topic.length > 0 && date.length > 0 && description.length > 0) {
+    if(topic.length != null && date.length != null && description.length != null && topic.length > 0 && date.length > 0 && description.length > 0) {
         localStorage.setItem("TOPIC", topic);
         localStorage.setItem("DATE", date);
         localStorage.setItem("DESCRIPTION", description);
